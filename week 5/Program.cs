a383476366f2e6a85a4e0bcee7e8f3557bf4e355
visualstudio <visualstudio@230-PC24>
2019-02-07 15:13:49 -0500

    3:09commit

--- week 5/Program.cs
+++ week 5/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace week_5
 {
-    class Program
-    {
-        static void Main(string[] args)
+   
+    
+        class Program
         {
+            static void Main(string[] args)
+            {
+                var a = new TestQuestion2();
+                a.PlayingWithForLoops();
+                Console.ReadLine();
+            }
         }
+        class TestQuestion2
+        {
+            public int myFavouiteVariable = 0;
+            public void PlayingWithForLoops()
+            {
+                // write a for loop to add 10 numbers
+                for (; MyMethod();)
+                {
+                    if (myFavouiteVariable > 10)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("i am in the for loop");
+                }
+            }
+            public bool MyMethod()
+            {
+                myFavouiteVariable++;
+                return true;
+            }
+        }
+
+    class dog
+    {
+        public string dog_name;
+        public string dog_breed;
+        public dog next_dog;
+        public dog prev_dog;
     }
-}
+    class birthday_party
+    {
+        public dog fifi;
+        public dog clearnace;
+        public dog head;
+        public dog tail;
+        public dog bruno;
+    }
+    }
